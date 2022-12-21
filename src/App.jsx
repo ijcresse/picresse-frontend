4140fbe8da93da5f06b54733cf62d27a5e062f24
Ian Cresse <ijcresse@gmail.com>
2022-12-20 18:34:18 -0800

    page now enforces a consistant size for unity container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Unity, useUnityContext } from 'react-unity-webgl';
 
 import Topbar from './components/Topbar.jsx';
